5cfa1d1bf8c0ac68e52bae4bcafbb6065937bfe0
RebonMaster <magik_smith@outlook.com>
2020-01-24 12:49:59 -0600

    first commit!

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TerrainGenerator
+{
+    public class MapGenerator : MonoBehaviour
+    {
+        public int mapWidth;
+        public int mapHeight;
+        public float noiseScale;
+
+        public bool autoUpdate;
+
+        public void GenerateMap()
+        {
+            float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale, 1,1,1);
+
+            MapDisplay display = FindObjectOfType<MapDisplay>();
+            display.DrawNoiseMap(noiseMap);
+        }
+    }
+}
